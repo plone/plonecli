83b011ef673ea680b44f423786621e943b0dbb60
Thomas Massmann <thomas.massmann@it-spir.it>
2017-10-21 15:20:35 +0200

    Update command skeletons.

--- plonecli/cli.py
+++ plonecli/cli.py
@@ -11,22 +11,57 @@ def bobtemplates():
 
 
 @bobtemplates.command()
-def create():
-    """Create a new Plone package.
-
-    $ mrbob plone_addon -O collective.todo [-v]
-    """
+@click.option('-v', '--verbose', is_flag=True)
+def create(verbose):
+    """Create a new Plone package."""
+    click.echo('create called')
+    if verbose:
+        click.echo('with verbose param')
 
 
 @bobtemplates.command()
-def add():
-    """Add a sub template to your existing package.
+@click.option('-v', '--verbose', is_flag=True)
+def add(verbose):
+    """Add a sub template to your existing package."""
+    click.echo('add called')
+    if verbose:
+        click.echo('with verbose param')
 
-    $ mrbob theme [-v]
-    """
+
+@click.group()
+def package():
+    """Package maintaining group."""
+    pass
 
 
-main = click.CommandCollection(sources=[bobtemplates])
+@package.command('virtualenv')
+def create_virtualenv():
+    """Create or update the local virtual environment."""
+    click.echo('virtualenv called')
+
+
+@package.command('requirements')
+def install_requirements():
+    """Install the local package requirements."""
+    click.echo('requirements called')
+
+
+@package.command('build')
+def run_buildout():
+    """Run the package buildout."""
+    click.echo('build called')
+
+
+@package.command('install')
+@click.pass_context
+def install_package(ctx):
+    """Install the package."""
+    ctx.forward(create_virtualenv)
+    ctx.forward(install_requirements)
+    ctx.forward(run_buildout)
+
+
+main = click.CommandCollection(sources=[bobtemplates, package])
 
 
 if __name__ == "__main__":
