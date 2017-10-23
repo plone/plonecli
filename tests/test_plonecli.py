5508f23dff11a9b14f3e3d5934db4a3ad901b49d
Thomas Massmann <thomas.massmann@it-spir.it>
2017-10-23 09:45:25 +0200

    Fix tests for plonecli.

--- tests/test_plonecli.py
+++ tests/test_plonecli.py
@@ -29,9 +29,9 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.cli)
     assert result.exit_code == 0
     assert 'Plone Command Line Interface' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(cli.cli, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
