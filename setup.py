901a3ca116df613e1993db93a5a53ee6a8025856
MrTango <md@derico.de>
2017-10-24 18:05:03 +0200

    Preparing release 0.1.0a3

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='plonecli',
-    version='0.1.0a3.dev0',
+    version='0.1.0a3',
     description="A Plone CLI for creating Plone packages",
     long_description=readme + '\n\n' + history,
     author="Maik Derstappen",
