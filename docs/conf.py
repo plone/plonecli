ff26860899c62a0c82057c13c70b42824f7f6cee
david <david@alteroo.com>
2017-11-01 08:56:21 -0500

    added the logo to the front page and docs

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the
 # top of the sidebar.
-#html_logo = None
+html_logo = 'plone_cli_logo.svg'
 
 # The name of an image file (within the static path) to use as favicon
 # of the docs.  This file should be a Windows icon file (.ico) being
