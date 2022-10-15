e0080fc0115ae510f779900cc6d43e5da7922c7e
MrTango <md@derico.de>
2022-10-15 15:07:53 +0200

    update tox setup and linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
 import os
 import pytest
 import sys
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def plonecli_bin():
     bin_path = os.path.abspath(os.path.dirname(sys.argv[0]))
-    plonecli_bin = bin_path + '/plonecli'
-    print('plonebin path: ' + plonecli_bin)
+    plonecli_bin = bin_path + "/plonecli"
+    print("plonebin path: " + plonecli_bin)
     yield plonecli_bin
--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 """Tests for `plonecli` package."""
 
@@ -16,14 +15,14 @@ def test_list_templates(tmpdir):
     os.chdir(tmpdir.strpath)
     reg = TemplateRegistry()
     template_str = reg.list_templates()
-    assert '- buildout' in template_str
-    assert '- addon' in template_str
-    assert '- theme' in template_str
-    assert '- content_type' in template_str
+    assert "- buildout" in template_str
+    assert "- addon" in template_str
+    assert "- theme" in template_str
+    assert "- content_type" in template_str
 
 
 def test_get_package_root(tmpdir):
-    target_dir = tmpdir.strpath + '/collective.foo'
+    target_dir = tmpdir.strpath + "/collective.foo"
     os.mkdir(target_dir)
 
     package_root = get_package_root()
@@ -34,7 +33,7 @@ def test_get_package_root(tmpdir):
 check=True
 
 """
-    with open(os.path.join(target_dir + '/bobtemplate.cfg'), 'w') as f:
+    with open(os.path.join(target_dir + "/bobtemplate.cfg"), "w") as f:
         f.write(template)
 
     package_root = get_package_root()
@@ -48,7 +47,7 @@ check=True
 template=plone_addon
 version=5.1-latest
 """
-    with open(os.path.join(target_dir + '/bobtemplate.cfg'), 'w') as f:
+    with open(os.path.join(target_dir + "/bobtemplate.cfg"), "w") as f:
         f.write(template)
 
     package_root = get_package_root()
@@ -56,7 +55,7 @@ version=5.1-latest
 
 
 def test_bob_config(tmpdir):
-    target_dir = tmpdir.strpath + '/collective.foo'
+    target_dir = tmpdir.strpath + "/collective.foo"
     os.mkdir(target_dir)
     os.chdir(target_dir)
     template = """[main]
@@ -64,16 +63,15 @@ template=plone_addon
 version=5.1-latest
 python=python2.7
 """
-    with open(os.path.join(target_dir + '/bobtemplate.cfg'), 'w') as f:
+    with open(os.path.join(target_dir + "/bobtemplate.cfg"), "w") as f:
         f.write(template)
     reg = TemplateRegistry()
     bob_config = read_bob_config(reg.root_folder)
-    assert bob_config.python == 'python2.7'
+    assert bob_config.python == "python2.7"
 
 
 def test_get_subtemplates(tmpdir):
-    """ test get_templates inside of a package
-    """
+    """test get_templates inside of a package"""
     template = """[check-manifest]
 check=True
 
@@ -81,32 +79,30 @@ check=True
 template=plone_addon
 version=5.1-latest
 """
-    target_dir = tmpdir.strpath + '/collective.foo'
+    target_dir = tmpdir.strpath + "/collective.foo"
     os.mkdir(target_dir)
-    with open(os.path.join(target_dir + '/bobtemplate.cfg'), 'w') as f:
+    with open(os.path.join(target_dir + "/bobtemplate.cfg"), "w") as f:
         f.write(template)
     os.chdir(target_dir)
     reg = TemplateRegistry()
     templates = reg.get_templates()
-    assert 'content_type' in templates
-    assert 'theme' in templates
-    assert 'vocabulary' in templates
+    assert "content_type" in templates
+    assert "theme" in templates
+    assert "vocabulary" in templates
 
 
 def test_get_templates(tmpdir):
-    """ test get_templates outside of a package
-    """
+    """test get_templates outside of a package"""
     os.chdir(tmpdir.strpath)
     reg = TemplateRegistry()
     templates = reg.get_templates()
-    assert 'addon' in templates
-    assert 'buildout' in templates
+    assert "addon" in templates
+    assert "buildout" in templates
 
 
 def test_resolve_template_name(tmpdir):
-    """ test resolving template names from plonecli alias
-    """
-    plonecli_alias = 'addon'
+    """test resolving template names from plonecli alias"""
+    plonecli_alias = "addon"
     reg = TemplateRegistry()
     template_name = reg.resolve_template_name(plonecli_alias)
-    assert 'bobtemplates.plone:addon' == template_name
+    assert "bobtemplates.plone:addon" == template_name
