dda2b87314759968855536b09a5f5ae8c9ea42e0
MrTango <md@derico.de>
2025-05-30 17:02:59 +0200

    depend on new bobtemplates.plone>=7.0.0a1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "Click>=7.0",
         "click-aliases",
         "mr.bob",
-        "bobtemplates.plone>=6.0b15",
+        "bobtemplates.plone>=7.0.0a1",
         "tox",
     ],
     extras_require={
